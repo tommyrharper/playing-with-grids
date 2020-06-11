e59d70a3cee2746f98b9982e9d44531fbb541f54
tommyrharper <tommyrharper@gmail.com>
2020-06-11 20:50:38 +0100

    Add two new grids to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import GrommetGrid from './GrommetGrid'
+import GrommetGridFinite from './GrommetGridFinite'
+import GrommetGridInfinite from './GrommetGridInfinite'
 
 class App extends React.Component {
   render() {
-    return <div>
-      <h1 >Hi, I'm Tom</h1>
-      <GrommetGrid/>
+    return(
+      <div>
+        <h1>Hi, I'm Tom</h1>
+        <GrommetGridFinite/>
+        <GrommetGridInfinite/>
       </div>
+    ) 
   }
 }
 
