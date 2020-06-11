b3fd1ec37b0361501975f75a48b6a5260ff09616
tommyrharper <tommyrharper@gmail.com>
2020-06-11 21:09:41 +0100

    Add GrommetBox component to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import GrommetGrid from './GrommetGrid'
+// import GrommetGrid from './GrommetGrid'
 import GrommetGridFinite from './GrommetGridFinite'
 import GrommetGridInfinite from './GrommetGridInfinite'
+import GrommetBox from './GrommetBox'
+import GrommetOnClickBox from './GrommetOnClickBox'
 
 class App extends React.Component {
   render() {
     return(
       <div>
         <h1>Hi, I'm Tom</h1>
+        <GrommetOnClickBox/>
+        <GrommetBox/>
         <GrommetGridFinite/>
         <GrommetGridInfinite/>
       </div>
