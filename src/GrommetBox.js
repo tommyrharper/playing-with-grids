8426efcee6829da7e06db8a2dad31d008cf38ebc
tommyrharper <tommyrharper@gmail.com>
2020-06-11 21:15:38 +0100

    Clean up GrommetBox component

--- src/GrommetBox.js
+++ src/GrommetBox.js
@@ -40,37 +40,7 @@ const SimpleBox = () => (
 
 class GrommetBox extends React.Component {
   render() {
-    return(
-      <Grommet theme={grommet}>
-      <Box
-        direction="row-responsive"
-        justify="center"
-        align="center"
-        pad="xlarge"
-        background="dark-2"
-        gap="medium"
-      >
-        <Box
-          pad="large"
-          align="center"
-          background={{ color: 'light-2', opacity: 'strong' }}
-          round
-          gap="small"
-        >
-          <Attraction size="large" />
-          <Text>Party</Text>
-          <Anchor href="" label="Link" />
-          <Button label="Button" onClick={() => {}} />
-        </Box>
-        <Box pad="large" align="center" background="dark-3" round gap="small">
-          <Car size="large" color="light-2" />
-          <Text>Travel</Text>
-          <Anchor href="" label="Link" />
-          <Button label="Button" onClick={() => {}} />
-        </Box>
-      </Box>
-    </Grommet>
-    )
+    return SimpleBox()
   }
 }
 
