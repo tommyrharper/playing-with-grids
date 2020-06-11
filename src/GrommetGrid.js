715f1c423aa69d4b8ff4e4cdb34b2361f4aef642
tommyrharper <tommyrharper@gmail.com>
2020-06-11 20:29:57 +0100

    Create first Grommet Grid

---
+++ src/GrommetGrid.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Grid, Box } from 'grommet';
+
+class GrommetGrid extends React.Component {
+  render() {
+    return <Grid
+    rows={['xxsmall', 'xsmall']}
+    columns={['xsmall', 'small']}
+    gap="small"
+    alignContent="center"
+    areas={[
+      { name: 'header', start: [0, 0], end: [1, 0] },
+      { name: 'nav', start: [0, 1], end: [0, 1] },
+      { name: 'main', start: [1, 1], end: [1, 1] },
+    ]}
+  >
+    <Box gridArea="header" background="brand" />
+    <Box gridArea="nav" background="brand" />
+    <Box gridArea="main" background="brand" />
+  </Grid>
+  }
+}
+
+export default GrommetGrid;
