7413d766ecffcc245933f510d772f1d4c793f663
tommyrharper <tommyrharper@gmail.com>
2020-06-11 20:29:37 +0100

    Add first grommet grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import GrommetGrid from './GrommetGrid'
 
 class App extends React.Component {
   render() {
     return <div>
       <h1 >Hi, I'm Tom</h1>
+      <GrommetGrid/>
       </div>
   }
 }
