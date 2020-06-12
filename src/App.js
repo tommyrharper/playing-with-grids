f30584d67200774a04fb487ebd1ce98b78267ca2
tommyrharper <tommyrharper@gmail.com>
2020-06-12 13:19:39 +0100

    Add flexbox

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import GrommetGridFinite from './GrommetGridFinite'
 import GrommetGridInfinite from './GrommetGridInfinite'
 import GrommetBox from './GrommetBox'
 import GrommetOnClickBox from './GrommetOnClickBox'
+import FlexBox from './FlexBox'
 
 class App extends React.Component {
   render() {
     return(
       <div>
         <h1>Hi, I'm Tom</h1>
+        <FlexBox/>
         <GrommetOnClickBox/>
         <GrommetBox/>
         <GrommetGridFinite/>
