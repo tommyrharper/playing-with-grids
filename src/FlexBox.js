a1800d238b04dad2ceb92a19f062caafdf0ba5ed
tommyrharper <tommyrharper@gmail.com>
2020-06-12 13:19:45 +0100

    Create flexbox

---
+++ src/FlexBox.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class FlexBox extends React.Component {
+  render() {
+    return(
+      <div class="contain-flex">
+      <div class="flex-container">
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+        <div class="within-flex">1</div>
+      </div>
+      </div>
+    ) 
+  }
+}
+
+export default FlexBox;
