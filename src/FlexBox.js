59d47b76683f1f9ad9f42ed5542502faab033975
tommyrharper <tommyrharper@gmail.com>
2020-06-12 13:51:42 +0100

    Add header to FlexBox

--- src/FlexBox.js
+++ src/FlexBox.js
@@ -3,23 +3,26 @@ import React from 'react';
 class FlexBox extends React.Component {
   render() {
     return(
-      <div class="contain-flex">
-      <div class="flex-container">
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-        <div class="within-flex">1</div>
-      </div>
+      <div>
+        <h1>This is FlexBox</h1>
+          <div class="contain-flex">
+            <div class="flex-container">
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+              <div class="within-flex">1</div>
+            </div>
+          </div>
       </div>
     ) 
   }
