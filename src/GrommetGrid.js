20ad4ea05ad40b4f5fb00e571b4f628f12dac740
tommyrharper <tommyrharper@gmail.com>
2020-06-12 13:52:20 +0100

    Add header to GrommetGrid

--- src/GrommetGrid.js
+++ src/GrommetGrid.js
@@ -3,21 +3,27 @@ import { Grid, Box } from 'grommet';
 
 class GrommetGrid extends React.Component {
   render() {
-    return <Grid
-    rows={['xxsmall', 'xsmall']}
-    columns={['xsmall', 'small']}
-    gap="small"
-    alignContent="center"
-    areas={[
-      { name: 'header', start: [0, 0], end: [1, 0] },
-      { name: 'nav', start: [0, 1], end: [0, 1] },
-      { name: 'main', start: [1, 1], end: [1, 1] },
-    ]}
-  >
-    <Box gridArea="header" background="brand" />
-    <Box gridArea="nav" background="brand" />
-    <Box gridArea="main" background="brand" />
-  </Grid>
+    return(
+    <div>
+      <h1>This is GrommitGrid</h1>
+      <Grid
+        rows={['xxsmall', 'xsmall']}
+        columns={['xsmall', 'small']}
+        gap="small"
+        alignContent="center"
+        areas={[
+          { name: 'header', start: [0, 0], end: [1, 0] },
+          { name: 'nav', start: [0, 1], end: [0, 1] },
+          { name: 'main', start: [1, 1], end: [1, 1] },
+        ]}
+      >
+        
+        <Box gridArea="header" background="brand" />
+        <Box gridArea="nav" background="brand" />
+        <Box gridArea="main" background="brand" />
+    </Grid>
+  </div>
+    )
   }
 }
 
